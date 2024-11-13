5a2e466217301bac3cd579facfefe2ffa9cf8eed
luccafinotti <00001113174043sp@al.educacao.sp.gov.br>
2024-11-13 08:44:17 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Literatura',
+    'Quem escreveu o livro O Príncipe?',
+    'Quem escreveu o livro O Principe foi Maquíavel'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital de Portugal?',
+    'A capital da Portugal é Lisboa'
+)
+
+criaCartao(
+    'Matemática',
+    'Quanto é 1+1?',
+    '1+1 é igual a 2'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
